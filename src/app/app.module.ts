77a9601839c1e060c1f3d59bac136614ef6f700f
ilabartenev <ilia.bartenev@novardis.com>
2022-07-20 16:35:21 +0300

    added third attempt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
+import { ButtonComponent } from './first-step/button/button.component';
 import { FirstStepComponent } from './first-step/first-step.component';
 import { SecondStepComponent } from './second-step/second-step.component';
 import { FinishStepComponent } from './finish-step/finish-step.component';
 import { HeaderComponent } from './header/header.component';
+import { FirstAttemptComponent } from './first-step/button/first-attempt/first-attempt.component';
+import { SecondAttemptComponent } from './first-step/button/second-attempt/second-attempt.component';
+import { ThirdAttemptComponent } from './first-step/button/third-attempt/third-attempt.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,9 @@ import { HeaderComponent } from './header/header.component';
     SecondStepComponent,
     FinishStepComponent,
     HeaderComponent,
+    FirstAttemptComponent,
+    SecondAttemptComponent,
+    ThirdAttemptComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/first-step/button/first-attempt/first-attempt.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IVoteItem } from '../../../app.component';
+
+@Component({
+  selector: 'app-first-attempt',
+  templateUrl: './first-attempt.component.html',
+  styleUrls: ['./first-attempt.component.scss']
+})
+export class FirstAttemptComponent {
+  @Input()
+  public voteItems: IVoteItem[] | null = [] as IVoteItem[];
+
+  @Input()
+  public attemptCount: number = 0;
+
+  @Output()
+  public selectVoteOutput: EventEmitter<IVoteItem | null> = new EventEmitter<IVoteItem | null>();
+
+  public clickEvent(item: IVoteItem | null): void {
+    this.selectVoteOutput.emit(item);
+
+    this.voteItems?.forEach(i => {
+      if (i !== item) {
+        i.active = false;
+      } else {
+        item.active = true;
+      }
+    });
+  }
+
+}
